4c2b7ba61553ca654cd042ce32c1e3db60122a51
kdivesh <kdivesh@gmail.com>
2019-04-21 02:59:01 -0500

    Intial project commit

---
+++ src/main/java/com/kdivesh/devops/devops/web/WelcomController.java
@@ -0,0 +1,16 @@
+package com.kdivesh.devops.devops.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomController {
+
+	@RequestMapping("/")
+	public String welcome(Model model){
+		model.addAttribute("course", "DevOps");
+		return "index";
+		
+	}
+}
